b4b48204c1d5eb04a48e3ef6ca113b40e38d80ae
SophiaRauls <sophiarauls@gmail.com>
2024-11-09 22:47:35 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,40 +1,45 @@
 let Wasser = 0
-basic.forever(function () {
-    if (pins.analogReadPin(AnalogPin.C17) > 550) {
-        images.iconImage(IconNames.Chessboard).showImage(0)
-        Wasser = 1
-    } else {
-        if (pins.analogReadPin(AnalogPin.C17) < 350) {
-            images.iconImage(IconNames.No).showImage(0)
-            Wasser = 0
-        } else {
-            images.iconImage(IconNames.Happy).showImage(0)
-            Wasser = 0
-        }
-    }
-    if (pins.analogReadPin(AnalogPin.C16) > 450) {
-        pins.servoWritePin(AnalogPin.P1, 180)
-        pins.servoWritePin(AnalogPin.P2, 180)
-    }
-    if (pins.analogReadPin(AnalogPin.C16) < 415) {
-        pins.servoWritePin(AnalogPin.P1, 45)
-        pins.servoWritePin(AnalogPin.P2, 45)
-    }
-    basic.pause(500)
-})
+let Temparatur = 0
+let Feuchte = 0
 basic.forever(function () {
     if (Wasser == 0) {
         motors.motorPower(0)
-        basic.turnRgbLedOff()
+        basic.setLedColor(0x000000)
     } else if (Wasser == 1) {
         motors.motorPower(100)
         basic.setLedColor(0x00ffff)
         basic.pause(10000)
         motors.motorPower(0)
-        basic.turnRgbLedOff()
+        basic.setLedColor(0x000000)
         basic.pause(50000)
     } else {
         motors.motorPower(0)
-        basic.turnRgbLedOff()
+        basic.setLedColor(0x000000)
     }
+})
+basic.forever(function () {
+    Temparatur = pins.analogReadPin(AnalogPin.C16)
+    Feuchte = pins.analogReadPin(AnalogPin.C17)
+    basic.showNumber(Feuchte)
+    if (Feuchte > 550) {
+        basic.showIcon(IconNames.Chessboard)
+        Wasser = 1
+    } else {
+        if (Feuchte < 350) {
+            basic.showIcon(IconNames.No)
+            Wasser = 0
+        } else {
+            basic.showIcon(IconNames.Happy)
+            Wasser = 0
+        }
+    }
+    if (Temparatur > 450) {
+        pins.servoWritePin(AnalogPin.P1, 180)
+        pins.servoWritePin(AnalogPin.P2, 180)
+    }
+    if (Temparatur < 415) {
+        pins.servoWritePin(AnalogPin.P1, 45)
+        pins.servoWritePin(AnalogPin.P2, 45)
+    }
+    basic.pause(500)
 })
