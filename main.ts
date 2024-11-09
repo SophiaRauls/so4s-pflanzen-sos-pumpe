7621bee8e83eb34b4556b188d400410898b5f5ab
SophiaRauls <sophiarauls@gmail.com>
2024-11-09 23:24:51 +0100

    Pin P0 für Feuchte auf Callioe rev3

--- main.ts
+++ main.ts
@@ -10,11 +10,13 @@ for (let index = 0; index < 2; index++) {
     basic.pause(750)
 }
 basic.forever(function () {
-    if (pins.analogReadPin(AnalogPin.C17) > 550) {
+    Feuchte = pins.analogReadPin(AnalogPin.P0)
+    Temperatur = pins.analogReadPin(AnalogPin.C16)
+    if (Feuchte > 550) {
         images.iconImage(IconNames.Chessboard).showImage(0)
         Wasser = 1
     } else {
-        if (pins.analogReadPin(AnalogPin.C17) < 350) {
+        if (Feuchte < 350) {
             images.iconImage(IconNames.No).showImage(0)
             Wasser = 0
         } else {
@@ -22,19 +24,17 @@ basic.forever(function () {
             Wasser = 0
         }
     }
-    if (pins.analogReadPin(AnalogReadWritePin.C16) > 450) {
+    if (Temperatur > 450) {
         pins.servoWritePin(AnalogPin.P1, 180)
         pins.servoWritePin(AnalogPin.P2, 180)
     }
-    if (pins.analogReadPin(AnalogReadWritePin.C16) < 415) {
+    if (Temperatur < 415) {
         pins.servoWritePin(AnalogPin.P1, 45)
         pins.servoWritePin(AnalogPin.P2, 45)
     }
     basic.pause(500)
 })
 basic.forever(function () {
-    Temperatur = pins.analogReadPin(AnalogPin.C16)
-    Feuchte = pins.analogReadPin(AnalogPin.C17)
     if (Wasser == 0) {
         motors.motorPower(0)
         basic.turnRgbLedOff()
